5601018072dc47028acc59a8f58602d61c10f365
davidlmorton <davidlmorton@localhost>
2010-10-15 15:23:36 -0500

    Made a basic_api directory for the basic API functions to reside. Made load the first basic API function. Changed load so that it takes a file handle instead of a filename. settings_list_from_yaml now also takes a file handle added simple example

--- configui/basic_api/load.py
+++ configui/basic_api/load.py
@@ -1,6 +1,6 @@
 from configui.settings_list_from_yaml import settings_list_from_yaml
 from configui.configuration_object import ConfigurationObject
 
-def load(yaml_filename):
-    settings_list = settings_list_from_yaml(yaml_filename)
+def load(yaml_file):
+    settings_list = settings_list_from_yaml(yaml_file)
     return ConfigurationObject(settings_list)
